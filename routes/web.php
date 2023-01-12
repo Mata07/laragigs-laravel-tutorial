970503f3292fcd3275a6f224174cb40ba12de4f0
Mata07 <mmatisic7@gmail.com>
2023-01-12 01:16:34 +0100

    Inital commit and routes example

---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// vježba rute
+Route::get('/hello', function () {
+    return 'Hello world';
+});
+
+// vraća response sa text/html
+// Route::get('/hello', function () {
+//     return response('<h1>Hello world</h1>');
+// });
+
+// vraća response sa text/html, ali ga pretvara u plain sa headers(->)
+Route::get('/hello', function () {
+    return response('<h1>Hello world</h1>')
+        ->header('Content-Type', 'text/plain')  // pretvara u text/plain
+        ->header('foo', 'bar');                 // dodaje foo bar header
+});
+
+
+// rute sa wildcards {}
+// http://localhost:8000/posts/10
+// Route::get('/posts/{id}', function ($id) {
+//     return response('Post ' . $id);         //ispiše Post 10
+// })->where('id', '[0-9]+');      // constraint sa RegEx - samo brojevi
+
+
+Route::get('/posts/{id}', function ($id) {
+    //dd($id);                           // die and dump - debug helperi - pokaže id
+    // ddd($id);    // ne radi - prošireni debug view
+    return response('Post ' . $id);         //ispiše Post 10
+})->where('id', '[0-9]+');      // constraint sa RegEx - samo brojevi
+
+
+// search route/query string primjer
+// http://localhost:8000/search?name=Brad&city=Boston
+Route::get('/search', function (Request $request) {
+    return $request->name . ' ' . $request->city;
+});
+
+
+
+
+
+
