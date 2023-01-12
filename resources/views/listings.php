b7079ec4589d8f54cc8c406d542ad66ddfcd2beb
Mata07 <mmatisic7@gmail.com>
2023-01-12 02:37:58 +0100

    Finish View listings.php

--- resources/views/listings.php
+++ resources/views/listings.php
@@ -1,2 +1,9 @@
 <!-- Prvi primjer -->
 <h1><?php echo $heading; ?></h1>
+
+<?php foreach($listings as $listing): ?>
+    <h2><?php echo $listing['title']; ?></h2>
+    <p>
+        <?php echo $listing['description']; ?>
+    </p>
+<?php endforeach; ?>
--- routes/web.php
+++ routes/web.php
@@ -61,14 +61,34 @@ use Illuminate\Support\Facades\Route;
 // Views and passing data -----------------------------
 // nova home ruta
 // http://localhost:8000
+// Route::get('/', function () {
+//     // passing data as array
+//     // vrati view listings.php i pošalji mu data
+//     return view('listings', [
+//         'heading' => 'Latest Listings'
+//     ]);
+// });
+
+// http://localhost:8000
 Route::get('/', function () {
     // passing data as array
     // vrati view listings.php i pošalji mu data
     return view('listings', [
-        'heading' => 'Latest Listings'
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget odio mauris. Quisque consectetur velit sit amet nisl accumsan rutrum. Cras non felis non neque egestas auctor. Nam magna libero, porttitor quis arcu id, scelerisque varius velit. Suspendisse sed urna in leo euismod venenatis. Vivamus ex justo, tempor vitae venenatis et, consequat sit amet lorem. Vivamus eget erat laoreet, elementum arcu a, vestibulum eros. Nullam fringilla enim est, sit amet ultrices leo dapibus vel. Cras non magna nisi.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget odio mauris. Quisque consectetur velit sit amet nisl accumsan rutrum. Cras non felis non neque egestas auctor. Nam magna libero, porttitor quis arcu id, scelerisque varius velit. Suspendisse sed urna in leo euismod venenatis. Vivamus ex justo, tempor vitae venenatis et, consequat sit amet lorem. Vivamus eget erat laoreet, elementum arcu a, vestibulum eros. Nullam fringilla enim est, sit amet ultrices leo dapibus vel. Cras non magna nisi.'
+            ]
+        ]
     ]);
 });
-
 
 
 
